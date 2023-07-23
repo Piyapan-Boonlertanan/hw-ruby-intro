a84269dda8aed48c691b1cfa655c12ffa604d7f6
Piyapan-Boonlertanan <s6401012620056@email.kmutnb.ac.th>
2023-07-23 21:36:55 +0700

    ruby_intro

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,58 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+  sorted_arr = arr.sort.reverse
+  sum_of_two_largest = sorted_arr[0] + sorted_arr[1]
+  sum_of_two_largest
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  complements = {}
+  arr.each do |element|
+    complement = n - element
+    return true if complements[complement]
+    complements[element] = true
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if s.empty? || !s[0].match?(/[a-zA-Z]/)
+  s[0].match?(/^[b-df-hj-np-tv-z]/i)
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless s.match?(/^[01]+$/)
+  number = s.to_i(2)
+  number % 4 == 0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'ISBN number cannot be empty' if isbn.empty?
+    raise ArgumentError, 'Price must be greater than zero' if price <= 0
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', price)
+  end
 end
